c25dd8bd2b77cedde5a62a88f39d7e8d79f15f6d
xueep <xueep@jcinfo.online>
2019-07-29 16:57:22 +0800

    init

---
+++ main.js
@@ -0,0 +1,95 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, globalShortcut, ipcMain} = require('electron')
+const path = require('path')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow() {
+	// Create the browser window.
+	mainWindow = new BrowserWindow({
+		width: 200,
+		height: 200,
+		minWidth: 200,
+		minHeight: 200,
+		maxWidth: 200,
+		maxHeight: 200,
+		transparent: true,
+		// frame: false,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: true
+		}
+	})
+
+	// and load the index.html of the app.
+	mainWindow.loadFile('index.html')
+	mainWindow.setAlwaysOnTop(true)
+
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools()
+
+	// Emitted when the window is closed.
+	mainWindow.on('closed', function () {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null
+	})
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+	// 注册一个 'CommandOrControl+Shift+Left' 的全局快捷键
+	globalShortcut.register('CommandOrControl+Shift+Left', () => {
+		// console.log('CommandOrControl+Shift+Left is pressed')
+		if (mainWindow) {
+			mainWindow.webContents.send('lastPage', 'default')
+		}
+	})
+	// console.log(globalShortcut.isRegistered('CommandOrControl+Shift+Left'))
+
+	// 注册一个 'CommandOrControl+Shift+Right' 的全局快捷键
+	globalShortcut.register('CommandOrControl+Shift+Right', () => {
+		// console.log('CommandOrControl+Shift+Right is pressed')
+		if (mainWindow) {
+			mainWindow.webContents.send('nextPage', 'default')
+		}
+	})
+	// console.log(globalShortcut.isRegistered('CommandOrControl+Shift+Right'))
+
+	globalShortcut.register('CommandOrControl+Shift+S', () => {
+		// console.log('CommandOrControl+Shift+Right is pressed')
+		if (mainWindow) {
+			mainWindow.webContents.send('nextPage', 'default')
+		}
+	})
+
+	ipcMain.on('print', (event, message) => {
+		console.clear()
+		console.log(message)
+	})
+
+	createWindow()
+
+
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) createWindow()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
